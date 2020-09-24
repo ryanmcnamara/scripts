1f0816c1412b0f61bf8899b3c1661c4fb281e3a4
Ryan McNamara <ryan.mcnamara@datadoghq.com>
2020-09-24 17:15:33 -0600

    add dr script

--- gcp/safe-scale-up-mig.sh
+++ gcp/safe-scale-up-mig.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+tmpFile="/tmp/dr_recovery.txt"
+
 function scaleupmig {
     name=$1
     zone=$2
@@ -28,5 +30,29 @@ function scaleupnodegroup {
 
     for migName migZone in $(kubectl get nodegroups -n $namespace  $name   -o json | jq -r '.status.gcp[].instanceGroupManager | "\(.name) \(.zone)"'); do
         scaleupmig $migName $migZone $sizePerZone
+    done
+}
+
+function setallnodegroupsizes {
+    rm -f $tmpFile
+    sourceFile=$1
+    for i in $(seq 1 $(cat "$sourceFile" | jq '.items | length')); do
+        cat "$sourceFile" | jq '.items['"$i"']' --compact-output > $tmpFile
+        date
+        name=$(cat $tmpFile | jq '.metadata.name')
+        namespace=$(cat $tmpFile | jq '.metadata.namespace')
+        echo namespace $namespace
+        echo name $name
+
+        largestSize=$(cat $tmpFile | jq -r '.status.gcp[].instanceGroupManager.targetSize' | sort -g | tail -n 1)
+
+        if [ ! kubectl get nodegroup -n $namespace $name > /dev/null 2>&1 ]; then
+            echo "nodegroup not found, will take no action, namespace: $namespace, name: $name"
+        else
+            echo "EXITING AS A SAFEGUARD, remove this exit to take action"
+            exit 1
+            echo "Scaling nodegroup -n $namespace $name to $largestSize per zone"
+            scaleupnodegroup $namespace $name $largestSize
+        fi
     done
 }
