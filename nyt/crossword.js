7502f2c06b4be50fae81b3757592830662185cb0
ryanmcnamara <30ryanm@gmail.com>
2023-02-13 21:42:36 -0700

    nyt

--- nyt/crossword.js
+++ nyt/crossword.js
@@ -1,1 +1,10 @@
+// ==UserScript==
+// @name         NYT_CROSSWORD_FIXES
+// @namespace    http://tampermonkey.net/
+// @version      0.1.0
+// @description  New york times crossword site fixes
+// @author       ryan.mcnamara
+// @match        https://*.nytimes.com/*
+// https://gist.github.com/samsungstark/7440c802b89f9ed413631b84cfb06cfe/edit
+
 document.getElementById("boardTitle").remove()
